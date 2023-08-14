f671ebe829c237dcd010cc69566af815230856bf
nimixx <nimixx.dev@proton.me>
2023-08-14 21:17:44 +0200

    Fix issue with tooltip on resume

--- src/components/header/Resume.tsx
+++ src/components/header/Resume.tsx
@@ -1,19 +1,19 @@
 import { Paperclip } from 'lucide-react'
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
-import Link from '../ui/link'
 
 export default function Resume() {
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
-          <Link
+          <a
             className=" hidden h-8 w-8 items-center justify-center rounded-md border border-zinc-200 text-sm font-medium ring-offset-white transition-colors hover:bg-zinc-200 hover:text-zinc-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-zinc-700 dark:text-indigo-400 dark:ring-offset-zinc-950  dark:hover:bg-zinc-700 dark:hover:bg-zinc-800/50 dark:focus-visible:ring-zinc-300 xl:inline-flex"
-            link="/"
+            href="/"
             target="_self"
+            rel="noopener noreferrer"
           >
             <Paperclip className="h-[1.2rem] w-[1.2rem]" />
-          </Link>
+          </a>
         </TooltipTrigger>
         <TooltipContent>
           <p>Ukázat Resume</p>
@@ -24,4 +24,3 @@ export default function Resume() {
 }
 
 //TODO: add resume and change target to _blank
-//TODO: Fix tooltip
