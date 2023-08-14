51f0643d865b5bcb4963d1cc9d461057ca33974a
nimixx <nimixx.dev@proton.me>
2023-08-14 23:23:38 +0200

    Add tooltip for skills

--- src/components/stuck/IconStuck.tsx
+++ src/components/stuck/IconStuck.tsx
@@ -1,33 +1,43 @@
-import { Icon } from '@iconify/react';
+import { Icon } from '@iconify/react'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip'
 
 const skills = [
-  'skill-icons:html',
-  'skill-icons:css',
-  'skill-icons:javascript',
-  'skill-icons:typescript',
-  'skill-icons:sass',
-  'skill-icons:react-dark',
-  'skill-icons:nextjs-dark',
-  'skill-icons:astro',
-  'skill-icons:tailwindcss-dark',
-  'skill-icons:emotion-dark',
-  'skill-icons:materialui-dark',
-  'skill-icons:graphql-dark',
-  'skill-icons:redux',
-  'skill-icons:vercel-dark',
-  'skill-icons:bash-dark',
-  'skill-icons:git',
-  'skill-icons:figma-dark',
-];
+  { icon: 'skill-icons:html', tooltip: 'HTML' },
+  { icon: 'skill-icons:css', tooltip: 'CSS' },
+  { icon: 'skill-icons:javascript', tooltip: 'JavaScript' },
+  { icon: 'skill-icons:typescript', tooltip: 'TypeScript' },
+  { icon: 'skill-icons:sass', tooltip: 'Sass' },
+  { icon: 'skill-icons:react-dark', tooltip: 'React' },
+  { icon: 'skill-icons:nextjs-dark', tooltip: 'Next.js' },
+  { icon: 'skill-icons:astro', tooltip: 'Astro' },
+  { icon: 'skill-icons:tailwindcss-dark', tooltip: 'Tailwind CSS' },
+  { icon: 'skill-icons:emotion-dark', tooltip: 'Emotion' },
+  { icon: 'skill-icons:materialui-dark', tooltip: 'Material UI' },
+  { icon: 'skill-icons:graphql-dark', tooltip: 'GraphQl' },
+  { icon: 'skill-icons:redux', tooltip: 'Redux' },
+  { icon: 'skill-icons:vercel-dark', tooltip: 'Vercel' },
+  { icon: 'skill-icons:bash-dark', tooltip: 'Bash' },
+  { icon: 'skill-icons:git', tooltip: 'Git' },
+  { icon: 'skill-icons:figma-dark', tooltip: 'Figma' },
+]
 
 export default function IconStuck() {
   return (
-    <section className="flex flex-row flex-wrap items-start 2xl:justify-between gap-2 w-full pt-5">
+    <section className="flex w-full flex-row flex-wrap items-start gap-2 pt-5 2xl:justify-between">
       {skills.map((skill, index) => (
-        <div key={index} className="w-10 h-10">
-          <Icon icon={skill} width="100%" height="100%" />
-        </div>
+        <TooltipProvider key={index}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div className="h-10 w-10">
+                <Icon icon={skill.icon} width="100%" height="100%" />
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{skill.tooltip}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       ))}
     </section>
-  );
+  )
 }
