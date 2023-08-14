ca6a37becab498edc1ec1f2983b6a8725db6a716
nimixx <nimixx.dev@proton.me>
2023-08-14 21:16:23 +0200

    Format code

--- src/components/header/Resume.tsx
+++ src/components/header/Resume.tsx
@@ -8,7 +8,7 @@ export default function Resume() {
       <Tooltip>
         <TooltipTrigger asChild>
           <Link
-            className=" hidden h-8 w-8 items-center justify-center rounded-md border border-zinc-200 text-sm font-medium ring-offset-white transition-colors hover:bg-zinc-200 hover:text-zinc-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-zinc-700 dark:text-indigo-400 dark:ring-offset-zinc-950  dark:hover:bg-zinc-700 dark:focus-visible:ring-zinc-300 xl:inline-flex dark:hover:bg-zinc-800/50"
+            className=" hidden h-8 w-8 items-center justify-center rounded-md border border-zinc-200 text-sm font-medium ring-offset-white transition-colors hover:bg-zinc-200 hover:text-zinc-900 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-zinc-700 dark:text-indigo-400 dark:ring-offset-zinc-950  dark:hover:bg-zinc-700 dark:hover:bg-zinc-800/50 dark:focus-visible:ring-zinc-300 xl:inline-flex"
             link="/"
             target="_self"
           >
--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,26 +1,20 @@
-import React from "react";
+import React from 'react'
 
 interface LinkProps {
-  children?: React.ReactNode;
-  link?: string;
-  target?: string;
-  className?: string;
+  children?: React.ReactNode
+  link?: string
+  target?: string
+  className?: string
 }
 
 const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
   ({ children, link, target, className }, ref) => {
     return (
-      <a
-        className={className}
-        href={link}
-        target={target}
-        rel="noopener noreferrer"
-        ref={ref}
-      >
+      <a className={className} href={link} target={target} rel="noopener noreferrer" ref={ref}>
         {children}
       </a>
-    );
-  }
-);
+    )
+  },
+)
 
-export default Link;
+export default Link
