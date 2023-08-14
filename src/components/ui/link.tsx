486035424839fce0d61ce0aeab329b0822b6b937
nimixx <nimixx.dev@proton.me>
2023-08-14 21:15:47 +0200

    Fix ref issue with link component

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,14 +1,26 @@
+import React from "react";
+
 interface LinkProps {
-  children?: React.ReactNode
-  link?: string
-  target?: string
-  className?: string
+  children?: React.ReactNode;
+  link?: string;
+  target?: string;
+  className?: string;
 }
 
-export default function Link({ children, link, target, className }: LinkProps) {
-  return (
-    <a className={className} href={link} target={target} rel="noopener noreferrer">
-      {children}
-    </a>
-  )
-}
+const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
+  ({ children, link, target, className }, ref) => {
+    return (
+      <a
+        className={className}
+        href={link}
+        target={target}
+        rel="noopener noreferrer"
+        ref={ref}
+      >
+        {children}
+      </a>
+    );
+  }
+);
+
+export default Link;
