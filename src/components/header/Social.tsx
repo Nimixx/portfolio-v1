b45967d3f3fd3b83a32a581a47eb2e7baa2c9861
nimixx <nimixx.dev@proton.me>
2023-08-15 08:11:30 +0200

    Add label for social link

--- src/components/header/Social.tsx
+++ src/components/header/Social.tsx
@@ -5,17 +5,17 @@ export default function Social() {
   return (
     <ul className=" flex items-center gap-2 text-gray-500 dark:text-gray-400">
       <li>
-        <Link link="https://github.com/Nimixx" target="_blank">
+        <Link link="https://github.com/Nimixx" target="_blank" ariaLabel="Visit Nimixx on GitHub">
           <Github className="h-4 w-4 hover:text-zinc-950 dark:hover:text-white sm:h-5 sm:w-5" />
         </Link>
       </li>
       <li>
-        <Link link="https://twitter.com/NimixxDev" target="_blank">
+        <Link link="https://twitter.com/NimixxDev" target="_blank" ariaLabel="Visit Nimixx on Twitter">
           <Twitter className="h-4 w-4 hover:text-zinc-950 dark:hover:text-white sm:h-5 sm:w-5" />
         </Link>
       </li>
       <li>
-        <Link link="https://www.linkedin.com/in/tadeasthelen/" target="_blank">
+        <Link link="https://www.linkedin.com/in/tadeasthelen/" target="_blank" ariaLabel="Visit Nimixx on LinkedIn">
           <Linkedin className="h-4 w-4 hover:text-zinc-950 dark:hover:text-white sm:h-5 sm:w-5" />
         </Link>
       </li>
--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,20 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 interface LinkProps {
-  children?: React.ReactNode
-  link?: string
-  target?: string
-  className?: string
+  children?: React.ReactNode;
+  link?: string;
+  target?: string;
+  className?: string;
+  ariaLabel?: string; // Add an ariaLabel prop
 }
 
 const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ children, link, target, className }, ref) => {
+  ({ children, link, target, className, ariaLabel }, ref) => {
     return (
-      <a className={className} href={link} target={target} rel="noopener noreferrer" ref={ref}>
+      <a
+        className={className}
+        href={link}
+        target={target}
+        rel="noopener noreferrer"
+        ref={ref}
+        aria-label={ariaLabel} // Set the aria-label attribute
+      >
         {children}
       </a>
-    )
-  },
-)
+    );
+  }
+);
 
-export default Link
+export default Link;
