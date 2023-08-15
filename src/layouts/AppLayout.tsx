38badbb14f174ffaa426de02db9ef29fb1c72784
Nimixx <ttadeasthelen@gmail.com>
2023-08-15 16:06:33 +0200

    Remove some style

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -7,7 +7,7 @@ interface AppLayoutProps {
 
 export default function AppLayout({ children }: AppLayoutProps) {
   return (
-    <article className="scrollbar-light min-h-screen w-full bg-zinc-100 text-zinc-900 balance dark:scrollbar-dark dark:bg-zinc-950 dark:text-zinc-100">
+    <article className="scrollbar-light min-h-screen w-full  text-zinc-900 balance dark:scrollbar-dark  dark:text-zinc-100">
       <Header />
       {children}
       <Footer />
