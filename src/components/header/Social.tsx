a1b4101a0bb786cbd5631ae21d68cfa8733aabb6
Nimixx <ttadeasthelen@gmail.com>
2023-08-15 11:49:40 +0200

    Fix some little issue and style

--- src/components/header/Social.tsx
+++ src/components/header/Social.tsx
@@ -6,17 +6,17 @@ export default function Social() {
     <ul className=" flex items-center gap-2 text-gray-500 dark:text-gray-400">
       <li>
         <Link link="https://github.com/Nimixx" target="_blank" ariaLabel="Visit Nimixx on GitHub">
-          <Github className="h-4 w-4 hover:text-zinc-950 dark:hover:text-white sm:h-5 sm:w-5" />
+          <Github className="h-5 w-5 hover:text-zinc-950 dark:hover:text-white sm:h-5 sm:w-5" />
         </Link>
       </li>
       <li>
         <Link link="https://twitter.com/NimixxDev" target="_blank" ariaLabel="Visit Nimixx on Twitter">
-          <Twitter className="h-4 w-4 hover:text-zinc-950 dark:hover:text-white sm:h-5 sm:w-5" />
+          <Twitter className="h-5 w-5 hover:text-zinc-950 dark:hover:text-white sm:h-5 sm:w-5" />
         </Link>
       </li>
       <li>
         <Link link="https://www.linkedin.com/in/tadeasthelen/" target="_blank" ariaLabel="Visit Nimixx on LinkedIn">
-          <Linkedin className="h-4 w-4 hover:text-zinc-950 dark:hover:text-white sm:h-5 sm:w-5" />
+          <Linkedin className="h-5 w-5 hover:text-zinc-950 dark:hover:text-white sm:h-5 sm:w-5" />
         </Link>
       </li>
     </ul>
